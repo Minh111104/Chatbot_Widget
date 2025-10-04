7b278d27fa00bc62af263b58b2b4a86944950578
Quang Minh <quangminh111104@gmail.com>
2025-10-04 06:56:03 -0400

    fix: make background transparent for embedding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatWidget from '@/components/ChatWidget'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
+    <main className="relative">
       <ChatWidget />
     </main>
   )
